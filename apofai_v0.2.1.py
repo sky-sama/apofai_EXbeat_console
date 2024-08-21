4cfa1de766fc995c057f6e8a5ffe82742d6bbccf
sky-sama <1834840@qq.com>
2024-08-21 12:52:43 +0800

    Update apofai_v0.2.5.py

--- apofai_v0.2.1.py
+++ apofai_v0.2.1.py
@@ -3,18 +3,20 @@ from matplotlib.widgets import Slider
 #import matplotlib.font_manager
 import numpy as np
 import scipy.io.wavfile as wav
-from scipy.fft import rfft, rfftfreq,fft,fftfreq,ifft
+from scipy.fft import fft,ifft
 import os.path as pth
 #from scipy.ndimage import convolve1d as convolve
 #import multiprocessing as process
 #import threading
 #import time
 import scipy.signal
+import subprocess
 #SAMPLE_RATE = 44100
 #DURATION = 5
 SIGMA=1e-2
 MU=0.5
 rate=None;all_samples=None;path=None;name=None;val=0;x=None;y0=None;y1=None;bpm=None
+height=[0,32767];threshold=[0,32767];distance=0
 
 def convfft(a, b):#使用快速傅里叶变换的快速卷积
     N = len(a)
@@ -31,50 +33,57 @@ plt.rcParams['font.family']=['Microsoft YaHei']#给plot库导入中文字体
 
 #指令
 #怀疑开头的卡顿都用在def上了（解释性语言）
+#现在想来应该是编译需要导入的库……？
 
 def cinfo(args):
-  print("自动采音器v0.2 Console.ver\n\
+  print("自动采音器v0.2.5 Console.ver\n\
 作者：@bilibili自己想柠檬\n\
 关注柠檬喵~关注柠檬谢谢喵~\n\
 --------------------------------------------------\n\
-当前可用指令：\n\
-info\n\
-help (demo)\n\
-list (demo)\n\
-open/openfile\n\
-show\n\
-std/savetimedata (useless)\n\
-bpm\n\
-val\n\
-make/generate\n\
-exit\n\
+输入help获取基本的使用帮助\n\
+输入list获取支持的所有命令\n\
 要求路径时可以使用相对路径（应该可以）\n\
 --------------------------------------------------")
 
 def chelp(args):
-  print("待施工。至少先像cmd那样用下试试。\n除部分指令外，空格会分隔参数。\n需要参数的命令，将参数留空可查看该命令的帮助")
+  print("基本格式：\n\tcommand [arg(s)]\n需要参数的命令，将参数留空可查看该命令的帮助。除部分指令外，空格会分隔不同参数。\n无需参数的命令，输入任何值不影响其执行。可在list命令中查看该命令的描述\n使用list命令获取当前版本支持的所有命令")
 
 def clist(args):
-  print("命令列表：\n"+"\n".join(commandlist.keys())+"\n待施工。以后会加命令描述")
+  print("当前可用的所有命令：\n"+"\n".join(pair[0]+"\t"+pair[1][1] for pair in commandlist.items())+"\n")
 
-def copenfile(args):
+def copenfile(args,show=True):
   global rate,all_samples,path,name,val,x,y0,y1
-  if len(args)==0 or args[0].lower=="help":
+  if len(args)==0 or args[0].lower=="help" or args[0].lower=="/?":
     print("命令格式：\n\
-    openfile path\n\
+    {openfile|openonly} path\n\
 单参数命令。空格在该命令中不分隔参数。\n\
-打开一个WAV音频文件，然后查看其波形。\n\
-参数1：path：待打开的WAV文件路径。")
+打开一个媒体文件。如果不是.wav格式将自动调用ffmpeg转码，并将覆盖同名wav文件，请注意。\n\
+参数1：path：待打开的媒体文件路径。\n\
+示例：\n\
+open C:\\Users\\\\Desktop\\_hitsound.wav\n\
+打开文件并预览采音\n\
+openonly F:\\A Dance of Fire and Ice\\背景视频.mp4\n\
+抽取音频转码为.wav文件，但不打开采音预览界面。")
     return
   tmp=" ".join(args)
   if not pth.isfile(tmp):
     print("不是一个有效的路径，请重试")
     return
-  path=tmp
+  if tmp[-4:].lower()!=".wav":
+    tmp2=".".join(tmp.split(".")[:-1])+".wav"
+    ffmpeg = subprocess.Popen('ffmpeg -i '+f'\"{tmp}\" \"{tmp2}\"',stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out=ffmpeg.communicate("y".encode())
+    print(f"{out[0]}\nffmpeg输出，看不懂没关系，反正我也不懂（总之没看到error就行\n{out[1]}")
+    if not pth.isfile(tmp2):
+      print("不是一个可识别的音频格式。请确保路径指向音频文件或包含音频的视频文件")
+      return
+    path=tmp2
+  else:
+    path=tmp
   try:
     rate, all_samples = wav.read(path)
   except:
-    print("文件读取失败：不是可用的文件格式或其他意外。当前仅支持读取WAV格式的音频。")
+    print("文件读取失败：不是可用的文件格式或其他意外。请联系作者排查故障。")
     return
   print("采样率",rate,"采样总数",len(all_samples),"数据类型（无用）",all_samples.dtype)
   name=path.split("\\")[-1:][0]
@@ -82,65 +91,109 @@ def copenfile(args):
   y0 = np.array(all_samples)[:,0]
   y1 = np.int32(y0)**2
   y1 = np.int16((y1/y1.max())*32767)
-  cshow([])
+  if show:
+    cshow([])
+
+def copenonly(args):
+  copenfile(args,show=False)
 
 def cshow(args):
-  global rate,all_samples,path,name,val,x,y0,y1
+  global rate,all_samples,path,name,val,x,y0,y1,height
   if path==None:
     print("你还没有选中文件。使用open指令打开一个WAV文件先~")
     return
-  fig, ax = plt.subplots()
-  plt.subplots_adjust(bottom=0.25)
-  plt.title(f"{name}的采音预览")
-  plt.plot(x, y0)
-  plt.plot(x, y1)
-  plt.xlabel("时间(s)")
-  plt.axhline(y=0, color='k')
+  try:
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.25)
+    plt.title(f"{name}的采音预览")
+    plt.plot(x, y0)
+    plt.plot(x, y1)
+    plt.xlabel("时间(s)")
+    plt.axhline(y=0, color='k')
+  except Exception as err:
+    print(f"绘制图像时发生错误。可能是内存不足。\n{err}")
+    return
   sig=SIGMA*np.exp(val*0.5)
   norm=1/(np.sqrt(2*np.pi)*sig)*np.exp(-((x-MU*len(x)/rate)**2)/(2*sig**2))
   try:
     y2=convfft(np.int32(y1),norm)
-  except:
-    print("在处理音频时发生了错误。可能是内存不足。")
+  except Exception as err:
+    print(f"在处理音频时发生了错误。可能是内存不足。\n{err}")
     return
   y2 = np.int16((y2/y2.max())*32767)
-  peak=scipy.signal.find_peaks(y2)[0]
-  line=ax.plot(x, y2)[0]
-  dots=ax.plot(peak/rate,y2[peak],'o',markersize=1)[0]
+  peak=scipy.signal.find_peaks(y2,height)[0]
+  try:
+    minline=ax.axhline(y=height[0], color='k')
+    maxline=ax.axhline(y=height[1], color='k')
+    line=ax.plot(x, y2)[0]
+    dots=ax.plot(peak/rate,y2[peak],'o',markersize=1)[0]
+  except Exception as err:
+    print(f"绘制图像时发生错误。可能是内存不足。\n{err}")
+    return
   
-  axfreq = plt.axes([0.1, 0.1, 0.8, 0.05])
-  slider = Slider(
-      ax=axfreq,
+  axval = plt.axes([0.1, 0.14, 0.8, 0.02])
+  sliderval = Slider(
+      ax=axval,
       label="平滑度",
       valmin=-5,
       valmax=5,
       valinit=val,
       valstep=0.1
   )
+  axmin = plt.axes([0.1, 0.12, 0.8, 0.02])
+  slidermin = Slider(
+      ax=axmin,
+      label="阈值（小）",
+      valmin=0,
+      valmax=32767,
+      valinit=height[0],
+      valstep=1
+  )
+  axmax = plt.axes([0.1, 0.1, 0.8, 0.02])
+  slidermax = Slider(
+      ax=axmax,
+      label="阈值（大）",
+      valmin=0,
+      valmax=32767,
+      valinit=height[1],
+      valstep=1
+  )
   
-  def update(val):
+  def valupdate(val):
     sig=SIGMA*np.exp(val*0.5)
     norm=1/(np.sqrt(2*np.pi)*sig)*np.exp(-((x-MU*len(x)/rate)**2)/(2*sig**2))
     try:
       y2=convfft(np.int32(y1),norm)
-    except:
-      print("在处理音频时发生了错误。可能是内存不足。")
+    except Exception as err:
+      print(f"在处理音频时发生了错误。可能是内存不足。\n{err}")
       return
     y2 = np.int16((y2/y2.max())*32767)
-    peak=scipy.signal.find_peaks(y2)[0]
+    peak=scipy.signal.find_peaks(y2,[slidermin.val,slidermax.val])[0]
     line.set_ydata(y2)
     dots.set_data(peak/rate,y2[peak])
   
-  slider.on_changed(update)
-  ini=slider.val
+  def minupdate(min):
+    peak=scipy.signal.find_peaks(y2,[min,slidermax.val])[0]
+    minline.set_ydata([min,min])
+    dots.set_data(peak/rate,y2[peak])
+  def maxupdate(max):
+    peak=scipy.signal.find_peaks(y2,[slidermin.val,max])[0]
+    maxline.set_ydata([max,max])
+    dots.set_data(peak/rate,y2[peak])
+  
+  sliderval.on_changed(valupdate)
+  slidermin.on_changed(minupdate)
+  slidermax.on_changed(maxupdate)
+  ini=sliderval.val
 
   plt.show(block=True)
 
-  if not ini==slider.val:
-    val=slider.val
+  height=[slidermin.val,slidermax.val]
+  if not ini==sliderval.val:
+    val=sliderval.val
 
 def cstd(args):
-  global rate,all_samples,path,name,val,x,y0,y1
+  global rate,all_samples,path,name,val,x,y0,y1,height
   if path==None:
     print("你还没有选中文件。使用open指令打开一个WAV文件先~")
     return
@@ -152,20 +205,26 @@ def cstd(args):
     print("在处理音频时发生了错误。可能是内存不足。")
     return
   y2 = np.int16((y2/y2.max())*32767)
-  peak=scipy.signal.find_peaks(y2)[0]
+  peak=scipy.signal.find_peaks(y2,height)[0]
   offset=peak/rate
-  np.savetxt(f"{path}_{val}_timedata.txt",offset)
+  curbpm=60/(offset-np.concatenate(([0],offset[:-1])))
+  np.savetxt(f"{path}_{val}_timedata.txt",curbpm)
   print(f"文件已保存至{path}_{val}_timedata.txt")
 
 def cbpm(args):
   global bpm
-  if len(args)==0 or args[0].lower=="help":
+  if len(args)==0 or args[0].lower=="help" or args[0].lower=="/?":
     print(f"命令格式：\n\
     bpm bpm\n\
 \n\
 重设输出谱面BPM，或者改为自动获取BPM。\n\
 参数1：bpm：将要设为的bpm。输入auto来使用自动获取（默认）\n\
-当前bpm：{bpm}")
+当前bpm：{bpm}\n\
+示例：\n\
+bpm 150\n\
+已将BPM设为150\n\
+bpm auto\n\
+BPM将自动测算")
     return
   try:
     bpm=float(args[0])
@@ -183,22 +242,54 @@ def cbpm(args):
 
 def cval(args):
   global val
-  if len(args)==0 or args[0].lower=="help":
+  if len(args)==0 or args[0].lower=="help" or args[0].lower=="/?":
     print(f"命令格式：\n\
-    val value\n\
-重设采音平滑度。可以突破滑动条的范围限制和分度值限制。\n\
-值越小采音强度越高，按键越密集。\n\
-参数1：value：将要设为的值。\n\
-当前平滑度：{val}")
+    val [value] [min] [max]\n\
+重设采音相关参数。\n\
+参数1：value：（可选的）采音平滑度。值越小采音强度越高，按键越密集。可以突破滑动条的范围限制和分度值限制。\n\
+参数2：min：（可选的）采音阈值（最小值）。低于此值则不采此音。只接受0~32767之间的整数值。\n\
+参数2：max：（可选的）采音阈值（最大值）。高于此值则不采此音。只接受0~32767之间的整数值。\n\
+当前平滑度：{val}\n\
+当前采音阈值：{height}\n\
+示例：\n\
+val -21 0 32767\n\
+已将平滑度设为-21\t已将采音阈值（最小值）设为0\t已将采音阈值（最大值）设为32767\n\
+val 0\n\
+已将平滑度设为0\n\
+val  100\n\
+已将采音阈值（最小值）设为100\n\
+val   32000\n\
+已将采音阈值（最大值）设为32000")
     return
-  try:
-    val=float(args[0])
-    print(f"已将平滑度设为{val}")
-  except ValueError:
-    print(f"无效输入。当前采音平滑度为{val}")
+  if not args[0]=="":
+    try:
+      val=float(args[0])
+      print(f"已将平滑度设为{val}")
+    except ValueError:
+      print(f"参数1无效。当前采音平滑度为{val}")
+  if not (len(args)<2 or args[1]==""):
+    try:
+      height[0]=np.int16(args[1])
+      print(f"已将采音阈值（最小值）设为{height[0]}")
+    except ValueError:
+      print(f"参数2无效。当前采音阈值（最小值）为{height[0]}")
+  if not (len(args)<3 or args[2]==""):
+    try:
+      height[1]=np.int16(args[2])
+      print(f"已将采音阈值（最大值）设为{height[1]}")
+    except ValueError:
+      print(f"参数3无效。当前采音阈值（最大值）为{height[1]}")
+
+def chz(args):
+  global y0,path
+  if path==None:
+    print("你还没有选中文件。使用open指令打开一个WAV文件先~")
+    return
+  peak=scipy.signal.find_peaks(-y0)[0]
+  make(peak,"hz")
 
 def cgenerate(args):
-  global rate,all_samples,path,name,val,x,y0,y1,bpm
+  global rate,all_samples,path,name,val,x,y0,y1,height
   if path==None:
     print("你还没有选中文件。使用open指令打开一个WAV文件先~")
     return
@@ -207,17 +298,20 @@ def cgenerate(args):
   
   y2=convfft(np.int32(y1),norm)
   y2 = np.int16((y2/y2.max())*32767)
-  peak=scipy.signal.find_peaks(y2)[0]
-  
+  peak=scipy.signal.find_peaks(y2,height)[0]
+  make(peak,val)
+
+def make(peak,exdescription):
+  global rate,path,bpm
   if len(peak)==0:
     print("你干嘛，怎么一个音也没采到，老子不干了！")
-    return
-  if len(peak)==1:
-    print("你干嘛，怎么只采到一个音，这让我怎么算BPM")
     return
   offset=peak/rate
   #np.savetxt(f"{name}_{val}_data.txt",offset)
   if bpm==None:
+    if len(peak)==1:
+      print("你干嘛，怎么只采到一个音，这让我怎么算BPM")
+      return
     offset0=offset[1:]-offset[:-1]
     bpmdt=np.median(offset0)
     bpm=np.int16(60/bpmdt)
@@ -344,9 +438,9 @@ def cgenerate(args):
   	[\n\
     ]\n\
   }"
-  with open(f"{path}_{val}.adofai","wt",1,"utf-8") as file:
+  with open(f"{path}_{exdescription}.adofai","wt",1,"utf-8") as file:
     file.write(tmpstr)
-    print(f"谱面文件已保存至{path}_{val}.adofai")
+    print(f"谱面文件已保存至{path}_{exdescription}.adofai")
 
 def cexit(args):
   exit()
@@ -354,13 +448,26 @@ def cexit(args):
 def celse(args):
   print("不是可用的命令。输入list获取当前支持的命令列表")
 
-commandlist={"info":cinfo,"help":chelp,"list":clist,"open":copenfile,"openfile":copenfile,"show":cshow,
-             "std":cstd,"savetimedata":cstd,"bpm":cbpm,"val":cval,"make":cgenerate,"generate":cgenerate,
-       		 "exit":cexit}
+commandlist={"info":(cinfo,"\t显示初始信息"),
+             "help":(chelp,"\t显示基本帮助"),
+             "list":(clist,"\t显示所有可用的命令列表（本指令）"),
+             "open":(copenfile,"\t=openfile"),
+             "openfile":(copenfile,"打开要处理的媒体文件并显示预览"),
+             "openonly":(copenonly,"仅打开文件而不显示预览（适合目的性强时）"),
+             "show":(cshow,"\t显示预览"),
+             "std":(cstd,"\t=savetimedata"),
+             "savetimedata":(cstd,"以curbpm形式存储采音信息（v0.2.2新）"),
+             "bpm":(cbpm,"\t查看或更改输出谱面BPM"),
+             "val":(cval,"\t查看或更改采音相关参数"),
+             "hz":(chz,"\t针对赫兹级采音优化的采音。跳过音高计算，直接生成.adofai谱面文件"),
+             "make":(cgenerate,"\t=generate"),
+             "generate":(cgenerate,"生成.adofai谱面文件"),
+       		   "exit":(cexit,"\t退出")}
 #Main:
 cinfo([])
 while True:
   command=input("=> ").strip().split(" ")
   if command[0]=="":
     continue
-  commandlist.get(command[0].lower(),celse)(command[1:])
+  commandlist.get(command[0].lower(),(celse,))[0](command[1:])
+#简洁。开心.jpg
